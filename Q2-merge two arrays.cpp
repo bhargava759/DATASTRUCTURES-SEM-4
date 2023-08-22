af5b6df64d84ed5f1e469ffe8ccad9598b622c39
bhargava759 <118883869+bhargava759@users.noreply.github.com>
2023-08-22 10:42:20 +0530

    Add files via upload

---
+++ Q2-merge two arrays.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+void mergeArrays(int arr1[], int size1, int arr2[], int size2, int mergedArr[]) {
+    int i = 0, j = 0, k = 0;
+    while (i < size1 && j < size2) {
+        if (arr1[i] < arr2[j]) {
+            mergedArr[k] = arr1[i];
+            i++;
+        } else {
+            mergedArr[k] = arr2[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < size1) {
+        mergedArr[k] = arr1[i];
+        i++;
+        k++;
+    }
+    while (j < size2) {
+        mergedArr[k] = arr2[j];
+        j++;
+        k++;
+    }
+}
+int main() {
+    int size1, size2;
+    printf("Enter the size of the first array: ");
+    scanf("%d", &size1);
+    int arr1[size1];
+    printf("Enter elements of the first array: ");
+    for (int i = 0; i < size1; i++) {
+        scanf("%d", &arr1[i]);
+    }
+    printf("Enter the size of the second array: ");
+    scanf("%d", &size2);
+    int arr2[size2];
+    printf("Enter elements of the second array: ");
+    for (int i = 0; i < size2; i++) {
+        scanf("%d", &arr2[i]);
+    }
+    int mergedSize = size1 + size2;
+    int mergedArr[mergedSize];
+    mergeArrays(arr1, size1, arr2, size2, mergedArr);
+    printf("Merged array: ");
+    for (int i = 0; i < mergedSize; i++) {
+        printf("%d ", mergedArr[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
